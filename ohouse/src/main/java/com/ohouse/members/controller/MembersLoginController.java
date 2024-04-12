0f7828c1eb64f361b20d12aeadd10c9fc06a11da
red-sprout <jjw05015@gmail.com>
2024-04-12 17:49:05 +0900

    [주재완] Feat: 로그인 폼으로 이동하는 서블릿

--- ohouse/src/main/java/com/ohouse/members/controller/MembersLoginController.java
+++ ohouse/src/main/java/com/ohouse/members/controller/MembersLoginController.java
@@ -1,11 +1,17 @@
 package com.ohouse.members.controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.ohouse.members.model.vo.Members;
+import com.ohouse.members.service.MembersServiceImpl;
 
 /**
  * Servlet implementation class MemberLoginController
@@ -25,8 +31,24 @@ public class MembersLoginController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.setCharacterEncoding("UTF-8");
+		
+		String membersId = request.getParameter("id");
+		String membersPwd = request.getParameter("password");
+		
+		Members m = new Members();
+		m.setMembersId(membersId);
+		m.setMembersPwd(membersPwd);
+		
+		Members loginUser = new MembersServiceImpl().loginMembers(m);
+		
+		if(loginUser == null) {
+			request.getSession().setAttribute("AlertMsg", "로그인에 실패하였습니다.");
+			response.sendRedirect(request.getContextPath());
+		} else {
+			request.getSession().setAttribute("loginUser", loginUser);
+			response.sendRedirect(request.getContextPath());
+		}
 	}
 
 	/**
