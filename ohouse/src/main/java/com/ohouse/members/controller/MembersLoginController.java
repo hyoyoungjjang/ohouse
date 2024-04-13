51c5358c3ab5211a9d125ffc90e1d2116c1d3d8a
red-sprout <jjw05015@gmail.com>
2024-04-14 01:15:14 +0900

    [주재완] Style: 불필요한 라이브러리 제거

--- ohouse/src/main/java/com/ohouse/members/controller/MembersLoginController.java
+++ ohouse/src/main/java/com/ohouse/members/controller/MembersLoginController.java
@@ -2,13 +2,11 @@ package com.ohouse.members.controller;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import com.ohouse.members.model.vo.Members;
 import com.ohouse.members.service.MembersServiceImpl;
